b00abe6d6f4f2aab826cac3f80ab43f511469588
Wolf-Martell Montwe <wolf@wolfmontwe.de>
2021-01-19 18:21:56 +0100

    Fix deployment target folder

--- buildSrc/src/main/kotlin/scripts/publishing-config.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/publishing-config.gradle.kts
@@ -37,7 +37,7 @@ publishing {
             }
         }
 
-        val target = "file://${project.buildDir}/gitPublish"
+        val target = "file://${rootProject.buildDir}/gitPublish"
 
         maven {
             name = "ReleasePackages"
