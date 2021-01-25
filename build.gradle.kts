1799fbd61d61f2795a5ff3e7d2080e3974049fe9
Wolf-Martell Montwe <wolf@wolfmontwe.de>
2021-01-25 11:37:12 +0100

    Change buildScript to use the new Gradle 6.8 feature to define a project wide repository setup in settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,19 +21,6 @@ plugins {
     id("scripts.publishing")
 }
 
-allprojects {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        google()
-        jcenter()
-
-        maven("https://kotlin.bintray.com/kotlin")
-        maven("https://kotlin.bintray.com/kotlinx")
-        maven("https://jitpack.io")
-    }
-}
-
 tasks.named<Wrapper>("wrapper") {
     gradleVersion = "6.8.1"
     distributionType = Wrapper.DistributionType.ALL
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,18 @@ rootProject.name = "hc-fhir-sdk-kmp"
 
 enableFeaturePreview("GRADLE_METADATA")
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+        jcenter()
+
+        maven("https://kotlin.bintray.com/kotlin")
+        maven("https://kotlin.bintray.com/kotlinx")
+        maven("https://jitpack.io")
+    }
+}
+
 include(
     ":sample-android",
 
